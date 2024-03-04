bf20e25d06bb23cb2a57228d42235cb2cb205da9
LexMartian <amirshok04@gmail.com>
2024-03-04 20:52:29 +0500

    lab 6

--- lab5/Regex_10.py
+++ lab5/Regex_10.py
@@ -4,7 +4,6 @@ def camel_to_snake(camel_case):
     snake_case = re.sub(r'(?<!^)(?=[A-Z])', '_', camel_case).lower()
     return snake_case
 
-# Test the function
 camel_case_string = "ThisIsCamelCaseString"
 snake_case_string = camel_to_snake(camel_case_string)
 print("Camel case string:", camel_case_string)
@@ -12,6 +11,5 @@ print("Snake case string:", snake_case_string)
 
 
 def insert_spaces(text):
-    # Use re.sub() to insert a space before each word starting with a capital letter
     modified_text = re.sub(r'([a-z])([A-Z])', r'\1_\2', text).lower()
     return modified_text
--- lab5/Regex_7.py
+++ lab5/Regex_7.py
@@ -1,8 +1,6 @@
 import re
 
-text = """
-snake_case_del cdef_adwwa _xacsc_csaas
-"""
+text = "snake_case_del cdef_adwwa _xacsc_csaas"
 pattern = r'_(\w)'  
 
 print(re.sub(pattern, lambda x: x.group(1).upper(), text))  
