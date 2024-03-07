e5da0b7325caf6442c14ae1d49fa81b70799ad53
LM <157613433+LexMartian@users.noreply.github.com>
2024-03-07 09:43:14 +0500

    Update Regex

--- lab5/Regex_6_replace.py
+++ lab5/Regex_6_replace.py
@@ -1,17 +1,14 @@
 import re
 
 def main():
-    file_path = r'C:\Users\User\Desktop\PP2_WWW\lab5\row.txt'  # Update with your file path
+    file_path = r'C:\Users\User\Desktop\PP2_WWW\lab5\row.txt' 
     with open(file_path, 'r', encoding='utf-8') as file:
-        file_content = file.read()  # Read the content of the file as a string
+        file_content = file.read()
         
-        # Define the regex pattern to match space, comma, or dot
         pattern = r'[ ,.]'
         
-        # Perform the substitution, replacing space, comma, or dot with a colon
         result = re.sub(pattern, ':', file_content)
         
-        # Print the modified content
         print("Modified content:")
         print(result)
         
